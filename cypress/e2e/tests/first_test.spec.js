f8b66bdabe38d0b7052d25644d784e4c95cdd1ab
Henil <henilp467@gmail.com>
2025-01-31 11:02:45 +1100

    [first_test.spec.js]: create the 1st test for asserting on expected heading text with actual heading text gotten from Cypress

--- cypress/e2e/tests/first_test.spec.js
+++ cypress/e2e/tests/first_test.spec.js
@@ -1,1 +1,13 @@
-;
+// Test heading appears with correct text on web page
+describe('Heading text', () => {
+    // Define all individual tests
+    it('contains the correct title', () => {
+        // Start Cypress commands
+        cy.visit('http://localhost:3000/example-1'); // URL of the web page for Cypress to visit
+
+        // make an assertion about the page visited
+        cy.get('h1') // find and get the 'h1' element on the page
+            .invoke('text') // the actual text present on the web page in the 'h1' element
+            .should('equal', 'My Awesome Web Application'); // the expected text that the heading should contain and be equal to above actual text found by .invoke()
+    });
+});
