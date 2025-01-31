ab4e02f5890226d42e8c79ff5d0cb68e71332b49
Henil <henilp467@gmail.com>
2025-01-31 12:21:36 +1100

    [selecting.spec.js]: use .eq(1) to specify the index of the <span> element from HTML tag

--- cypress/e2e/tests/selecting.spec.js
+++ cypress/e2e/tests/selecting.spec.js
@@ -5,21 +5,24 @@ describe('Text box with max characters', () => {
         cy.visit('http://localhost:3000/example-3');
 
         cy.get('span')
+            .eq(1) // argument is the index of the above element we want to select.
             .invoke('text') // get the actual text value of the span element
             .should('equal', '15'); // assert that the initial value of span text is expected to be 15 (all 15 chars left)
 
         // Have Cypress something in the text input box
-        cy.get('input').type('hello'); // 'hello' = 5 letters out of 15 used
+        cy.get('input').eq(1).type('hello'); // 'hello' = 5 letters out of 15 used
 
         // Now, expect the characters left text to tell us that we have 10 characters left (15-5)
         cy.get('span')
+            .eq(1)
             .invoke('text')
             .should('equal', '10'); // assert that actual text gotten from Cypress above is equal to expected text of 10 after 5 chars is reduced by 'hello'
 
-        cy.get('input').type(' my friend'); // use the rest of the 10 characters left
+        cy.get('input').eq(1).type(' my friend'); // use the rest of the 10 characters left
 
         // Finally, check that no characters are left after above text inputted
         cy.get('span')
+            .eq(1)
             .invoke('text')
             .should('equal', '0'); // assert that no chars are left to be typed as max limit is reached
     });
