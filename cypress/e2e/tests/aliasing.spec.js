03588eda3db6b28ba7e057923ef6d76cc9c4c998
Henil <henilp467@gmail.com>
2025-01-31 13:55:12 +1100

    [aliasing.spec.js]: create alias as charInput for the input field and use @charInput for all repetitive uses

--- cypress/e2e/tests/aliasing.spec.js
+++ cypress/e2e/tests/aliasing.spec.js
@@ -6,7 +6,11 @@ describe('Text box with max characters', () => {
 
         cy.get('[data-cy="last-name-chars-left-count"]')
             .as('charsLeftSpan'); // this whole command tells Cypress to look at the above data-cy value selection and we want to refer to it as 'charsLeftSpan' for all future uses
-        
+
+        // Create an alias for input field
+        cy.get('[data-cy="input-last-name"]')
+            .as('charInput');
+
         // So we can replace all instances and uses of the above cy.get() command as @charsLeftSpan
 
         cy.get('@charsLeftSpan') // copy and paste the data-cy attribute's value of the span element which has characters left amount
@@ -14,14 +18,14 @@ describe('Text box with max characters', () => {
             .should('equal', '15'); // assert that the initial value of span text is expected to be 15 (all 15 chars left)
 
         // Have Cypress something in the text input box
-        cy.get('[data-cy="input-last-name"]').type('hello'); // 'hello' = 5 letters out of 15 used
+        cy.get('@charInput').type('hello'); // 'hello' = 5 letters out of 15 used
 
         // Now, expect the characters left text to tell us that we have 10 characters left (15-5)
         cy.get('@charsLeftSpan')
             .invoke('text')
             .should('equal', '10'); // assert that actual text gotten from Cypress above is equal to expected text of 10 after 5 chars is reduced by 'hello'
 
-        cy.get('[data-cy="input-last-name"]').type(' my friend'); // use the rest of the 10 characters left
+        cy.get('@charInput').type(' my friend'); // use the rest of the 10 characters left
 
         // Finally, check that no characters are left after above text inputted
         cy.get('@charsLeftSpan')
@@ -34,11 +38,15 @@ describe('Text box with max characters', () => {
     it('prevents the user from typing more characters once max is exceeded', () => {
         cy.visit('http://localhost:3000/example-3');
 
+        // Redefine the alias for input field
+        cy.get('[data-cy="input-last-name"]')
+            .as('charInput');
+
         // Test the input element and type a long string over 15 characters
-        cy.get('[data-cy="input-last-name"]').type('abcdefghijklmnopqrstuvwxyz');
+        cy.get('@charInput').type('abcdefghijklmnopqrstuvwxyz');
 
         // Assertion
-        cy.get('[data-cy="input-last-name"]') // find and get the input element
+        cy.get('@charInput') // find and get the input element
             .should('have.attr', 'value', 'abcdefghijklmno'); // assert on the 'value' attribute of the <input> element found that it should contain exactly 15 chars from the actual string typed above
     });
 });
