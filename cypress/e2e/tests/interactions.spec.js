9ca0e03f8cb83fe89ba53fa567b78c2390b88d5d
Henil <henilp467@gmail.com>
2025-01-31 15:04:07 +1100

    create a new interactions.spec.js to experiment with interacting with various form elements in example-4 web page

---
+++ cypress/e2e/tests/interactions.spec.js
@@ -0,0 +1,5 @@
+describe('Basic page interactions', () => {
+    beforeEach(() => {
+        cy.visit('/example-4'); // this route contains a playground for the interaction commands (has double-clicking, checking and unchecking boxes, selecting from dropdown and triggering a mouse-over event)
+    });
+})
