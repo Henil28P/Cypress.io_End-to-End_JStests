4e997c53659e29e9f61c561125e395108c7ea25e
Henil <henilp467@gmail.com>
2025-01-31 11:25:51 +1100

    [max_char_input.spec.js]: implement tests to check text value of <span> of amount of chars left after inputting text

--- cypress/e2e/tests/max_char_input.spec.js
+++ cypress/e2e/tests/max_char_input.spec.js
@@ -7,5 +7,20 @@ describe('Text box with max characters', () => {
         cy.get('span')
             .invoke('text') // get the actual text value of the span element
             .should('equal', '15'); // assert that the initial value of span text is expected to be 15 (all 15 chars left)
+
+        // Have Cypress something in the text input box
+        cy.get('input').type('hello'); // 'hello' = 5 letters out of 15 used
+
+        // Now, expect the characters left text to tell us that we have 10 characters left (15-5)
+        cy.get('span')
+            .invoke('text')
+            .should('equal', '10'); // assert that actual text gotten from Cypress above is equal to expected text of 10 after 5 chars is reduced by 'hello'
+
+        cy.get('input').type(' my friend'); // use the rest of the 10 characters left
+
+        // Finally, check that no characters are left after above text inputted
+        cy.get('span')
+            .invoke('text')
+            .should('equal', '0'); // assert that no chars are left to be typed as max limit is reached
     });
 });
