94e13d0b409c0d3ecff2ad864d501dceb2ea6fb8
Henil <henilp467@gmail.com>
2025-01-31 13:31:35 +1100

    [selecting.spec.js]: change the use of <input> HTML tag to use data-cy attribute value of the <input> element in the cy.get() method

--- cypress/e2e/tests/selecting.spec.js
+++ cypress/e2e/tests/selecting.spec.js
@@ -9,14 +9,14 @@ describe('Text box with max characters', () => {
             .should('equal', '15'); // assert that the initial value of span text is expected to be 15 (all 15 chars left)
 
         // Have Cypress something in the text input box
-        cy.get('input').type('hello'); // 'hello' = 5 letters out of 15 used
+        cy.get('[data-cy="input-last-name"]').type('hello'); // 'hello' = 5 letters out of 15 used
 
         // Now, expect the characters left text to tell us that we have 10 characters left (15-5)
         cy.get('[data-cy="last-name-chars-left-count"]')
             .invoke('text')
             .should('equal', '10'); // assert that actual text gotten from Cypress above is equal to expected text of 10 after 5 chars is reduced by 'hello'
 
-        cy.get('input').type(' my friend'); // use the rest of the 10 characters left
+        cy.get('[data-cy="input-last-name"]').type(' my friend'); // use the rest of the 10 characters left
 
         // Finally, check that no characters are left after above text inputted
         cy.get('[data-cy="last-name-chars-left-count"]')
@@ -30,10 +30,10 @@ describe('Text box with max characters', () => {
         cy.visit('http://localhost:3000/example-3');
 
         // Test the input element and type a long string over 15 characters
-        cy.get('input').type('abcdefghijklmnopqrstuvwxyz');
+        cy.get('[data-cy="input-last-name"]').type('abcdefghijklmnopqrstuvwxyz');
 
         // Assertion
-        cy.get('input') // find and get the input element
+        cy.get('[data-cy="input-last-name"]') // find and get the input element
             .should('have.attr', 'value', 'abcdefghijklmno'); // assert on the 'value' attribute of the <input> element found that it should contain exactly 15 chars from the actual string typed above
     });
 });
